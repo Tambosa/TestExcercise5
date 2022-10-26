7c1e55f10fc770969f224b663ecf9785690cc1a8
Tambosa <tambosa@gmail.com>
2022-10-26 10:20:59 +0300

    загрузка последующих страниц

--- app/src/main/java/com/aroman/testexcercise5/ui/MainActivity.kt
+++ app/src/main/java/com/aroman/testexcercise5/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aroman.testexcercise5.data.RedditFeedApi
 import com.aroman.testexcercise5.data.RetrofitClient
 import com.aroman.testexcercise5.data.RetrofitRedditRepositoryImpl
@@ -29,15 +30,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.redditRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.redditRecyclerView.adapter = redditPostsAdapter
+        initRecyclerView()
+        initViewModel()
+    }
 
+    private fun initViewModel() {
         viewModel.pageList.observe(this) {
             Log.d("@@@", it.data.children.toString())
             redditPostsAdapter.addData(it.data.children)
         }
-        viewModel.getPage(PageKey(10, null, 10))
+        viewModel.getPage(getStartKey())
     }
+
+    private fun initRecyclerView() {
+        binding.redditRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.redditRecyclerView.adapter = redditPostsAdapter
+        binding.redditRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val layoutManager = binding.redditRecyclerView.layoutManager as LinearLayoutManager
+                val visibleItemCount = layoutManager.childCount
+                val totalItemCount = layoutManager.itemCount
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+                if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0) {
+                    loadNextPage()
+                }
+            }
+        })
+    }
+
+    private fun loadNextPage() {
+        viewModel.getPage(getNextKey())
+    }
+
+    private fun getStartKey() = PageKey(10, null, 10)
+
+    private fun getNextKey() = PageKey(10, redditPostsAdapter.getData().last().data.name, 10)
 
     private fun onItemClick(position: Int) {
         val browserIntent = Intent(
